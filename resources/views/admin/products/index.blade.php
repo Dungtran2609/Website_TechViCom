58e7d6378be0680aab0b4a694e71232ee571c658
TienHoang472005 <kurosama0407@gmail.>
2025-08-21 05:56:47 +0700

    thêm bộ lọc cho quản lý danh mục, thương hiệu, thuộc tính

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -63,6 +63,7 @@
                 <table class="table align-middle table-hover">
                     <thead class="bg-light-subtle">
                         <tr>
+                            <th>ID</th>
                             <th>Ảnh</th>
                             <th>Tên sản phẩm</th>
                             <th>Loại</th>
@@ -75,6 +76,7 @@
                     <tbody>
                         @forelse ($products as $product)
                             <tr>
+                                <td>{{ $product->id }}</td>
                                 <td>
                                     @if ($product->thumbnail && Storage::disk('public')->exists($product->thumbnail))
                                         <img src="{{ asset('storage/' . $product->thumbnail) }}" alt="{{ $product->name }}"
